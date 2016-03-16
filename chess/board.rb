c64ce0fbdf4667239ae89564d538f8cda63ca3a3
Scott Ries <scottmries@gmail.com>
2016-03-16 17:37:49 -0400

    DRY out Board#populate_grid.

--- chess/board.rb
+++ chess/board.rb
@@ -9,17 +9,17 @@ class Board
   end
 
   def populate_grid
-    self.grid[1].each_index{|i| self.grid[1][i] = Pawn.new(:black, self, [1,i])}
-    self.grid[6].each_index{|i| self.grid[6][i] = Pawn.new(:white, self, [6,i])}
-
-    self.grid[0][0], self.grid[0][7] = Rook.new(:black, self, [0,0]), Rook.new(:black, self, [0,7])
-    self.grid[7][0], self.grid[7][7] = Rook.new(:white, self, [7,0]), Rook.new(:white, self, [7,7])
-    self.grid[0][1], self.grid[0][6] = Knight.new(:black, self, [0,1]), Knight.new(:black, self, [0,6])
-    self.grid[7][1], self.grid[7][6] = Knight.new(:white, self, [7,1]), Knight.new(:white, self, [7,6])
-    self.grid[0][2], self.grid[0][5] = Bishop.new(:black, self, [0,2]), Bishop.new(:black, self, [0,5])
-    self.grid[7][2], self.grid[7][5] = Bishop.new(:white, self, [7,2]), Bishop.new(:white, self, [7,5])
-    self.grid[0][3], self.grid[7][3] = Queen.new(:black, self, [0,3]), Queen.new(:white, self, [7,3])
-    self.grid[0][4], self.grid[7][4] = King.new(:black, self, [0,4]), King.new(:white, self, [7,4])
+    pieces = [Pawn, Rook, Knight, Bishop, Queen, King]
+    piece_columns = [(0..7), [0, 7], [1, 6], [2, 5], [3], [4]]
+    pieces.each_with_index do |piece, idx|
+      row_colors = [{row: (piece == Pawn ? 1 : 0), color: :black},
+        {row: (piece == Pawn ? 6 : 7), color: :white}]
+      row_colors.each do |row_color|
+        piece_columns[idx].each do |column|
+          self.grid[row_color[:row]][column] = piece.new(row_color[:color], self, [row_color[:row], column])
+        end
+      end
+    end
   end
 
   def [](pos)
